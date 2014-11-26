c224027b3e30a916a6f219f87195488522a756ac
duviteck <duviteck@yandex-team.ru>
2014-11-27 00:29:36 +0300

    fixed Problem_C from Codeforces round 279 div 2

--- src/codeforces/round279_div2/Problem_C.java
+++ src/codeforces/round279_div2/Problem_C.java
@@ -1,15 +1,11 @@
 package codeforces.round279_div2;
 
-import java.math.BigInteger;
 import java.util.Scanner;
 
 /**
  * Created by duviteck. 23 Nov 2014.
  */
 public class Problem_C {
-
-    public static final String YES = "Yes";
-    public static final String NO = "NO";
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
@@ -18,36 +14,44 @@ public class Problem_C {
         String[] divs = s.nextLine().split(" ");
         s.close();
 
-        String a = divs[0];
-        String b = divs[1];
-        BigInteger aNum = new BigInteger(a);
-        BigInteger bNum = new BigInteger(b);
-
         if (numLine.length() < divs[0].length() + divs[1].length()) {
-            System.out.println(NO);
+            System.out.println("NO");
             return;
         }
 
-        char[] chars = numLine.toCharArray();
-        for (int i = a.length(); i <= numLine.length() - b.length(); i++) {
-            if (chars[i] == '0') {
+        int len = numLine.length();
+        int a = Integer.parseInt(divs[0]);
+        int b = Integer.parseInt(divs[1]);
+        int[] digits = new int[len];
+        for (int i = 0; i < len; i++) {
+            digits[i] = numLine.charAt(i) - '0';
+        }
+
+        int[] remA = new int[len];
+        remA[0] = digits[0] % a;
+        for (int i = 1; i < len - divs[1].length(); i++) {
+            remA[i] = (remA[i - 1] * 10 + digits[i]) % a;
+        }
+
+        int[] remB = new int[len];
+        remB[len - 1] = digits[len - 1] % b;
+        int pow = 1;
+        for (int i = len - 2; i >= divs[0].length(); i--) {
+            pow = (pow * 10) % b;
+            remB[i] = (digits[i] * pow + remB[i + 1]) % b;
+        }
+
+        for (int i = divs[0].length(); i <= len - divs[1].length(); i++) {
+            if ((digits[i] == 0) || remA[i - 1] != 0 || remB[i] != 0) {
                 continue;
             }
 
-            String aLine = numLine.substring(0, i);
-            BigInteger aBig = new BigInteger(aLine);
-            if (aBig.divideAndRemainder(aNum)[1].equals(BigInteger.ZERO)) {
-                String bLine = numLine.substring(i);
-                BigInteger bBig = new BigInteger(bLine);
-                if (bBig.divideAndRemainder(bNum)[1].equals(BigInteger.ZERO)) {
-                    System.out.println(YES);
-                    System.out.println(aLine);
-                    System.out.println(bLine);
-                    return;
-                }
-            }
+            System.out.println("YES");
+            System.out.println(numLine.substring(0, i));
+            System.out.println(numLine.substring(i));
+            return;
         }
 
-        System.out.println(NO);
+        System.out.println("NO");
     }
 }
