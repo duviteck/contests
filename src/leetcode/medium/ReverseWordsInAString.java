5b04034fdda9eb57899a1cb20a01505fc56ad40e
duviteck <duviteck@yandex-team.ru>
2014-12-02 13:33:21 +0300

    Added ReverseWordsInAString (medium) problem from Leetcode

---
+++ src/leetcode/medium/ReverseWordsInAString.java
@@ -0,0 +1,77 @@
+package leetcode.medium;
+
+/**
+ * Created by duviteck. 02 Dec 2014.
+ */
+public class ReverseWordsInAString {
+
+    public static void main(String[] args) {
+        ReverseWordsInAString instance = new ReverseWordsInAString();
+        System.out.println(instance.reverseWords("   a   b "));
+    }
+
+    public String reverseWords(String s) {
+        if (s == null) {
+            return null;
+        }
+
+        s = s.trim();
+        if (s.length() == 0) {
+            return s;
+        }
+
+        char[] chars = s.toCharArray();
+        reverse(chars, 0, chars.length - 1);
+
+        StringBuilder sb = new StringBuilder(chars.length);
+        int start = 0;
+        while (start < chars.length) {
+            if (sb.length() > 0) {
+                sb.append(' ');
+            }
+
+            int[] wordIndexes = findWord(chars, start);
+            int len = wordIndexes[1] - wordIndexes[0] + 1;
+            reverse(chars, wordIndexes[0], wordIndexes[1]);
+            sb.append(chars, wordIndexes[0], len);
+            start = wordIndexes[1] + 1;
+        }
+        return sb.toString();
+    }
+
+    private int[] findWord(char[] chars, int start) {
+        int[] res = new int[2];
+        boolean isWord = false;
+        for (int i = start; i < chars.length; i++) {
+            if (!isWord && chars[i] != ' ') {
+                res[0] = i;
+                isWord = true;
+            } else if (isWord && chars[i] == ' ') {
+                res[1] = i - 1;
+                return res;
+            }
+        }
+        if (isWord) {
+            res[1] = chars.length - 1;
+        } else {
+            res[0] = res[1] = -1;
+        }
+        return res;
+    }
+
+    private void reverse(char[] chars, int start, int end) {
+        int i1 = start;
+        int i2 = end;
+        while (i1 < i2) {
+            swap(chars, i1, i2);
+            i1++;
+            i2--;
+        }
+    }
+
+    private void swap(char[] ar, int pos1, int pos2) {
+        char temp = ar[pos1];
+        ar[pos1] = ar[pos2];
+        ar[pos2] = temp;
+    }
+}
