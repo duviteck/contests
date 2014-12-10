d6758304f39c6ae04322bbf2bc7f3e056c240f84
duviteck <duviteck@yandex-team.ru>
2014-12-11 00:55:19 +0300

    Added ValidateBinarySearchTree (medium) problem from Leetcode

---
+++ src/leetcode/medium/ValidateBinarySearchTree.java
@@ -0,0 +1,35 @@
+package leetcode.medium;
+
+/**
+ * Created by duviteck. 11 Dec 2014.
+ */
+public class ValidateBinarySearchTree {
+
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, null, null);
+    }
+
+    private boolean isValidBST(TreeNode node, Integer min, Integer max) {
+        if (node == null) {
+            return true;
+        }
+        if (min != null && node.val <= min) {
+            return false;
+        }
+        if (max != null && node.val >= max) {
+            return false;
+        }
+        return isValidBST(node.left, min, node.val) && isValidBST(node.right, node.val, max);
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
