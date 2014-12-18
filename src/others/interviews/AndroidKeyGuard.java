b1134718a9f328c13e612cfbfe3eec054cad12b9
duviteck <duviteck@yandex-team.ru>
2014-12-18 14:49:18 +0300

    finished and optimized AndroidKeyGuard problem from interviews

--- src/others/interviews/AndroidKeyGuard.java
+++ src/others/interviews/AndroidKeyGuard.java
@@ -20,9 +20,14 @@ public class AndroidKeyGuard {
 
     public static final int[] NEXT_FIRST = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
     public static final int[] NEXT_1 = new int[]{2, 4, 5, 6, 8};
-    public static final int[] NEXT_2 = new int[]{2, 4, 5, 6, 8};
+    public static final int[] NEXT_2 = new int[]{1, 3, 4, 5, 6, 7, 9};
     public static final int[] NEXT_3 = new int[]{2, 4, 5, 6, 8};
-    public static final int[] NEXT_4 = new int[]{2, 4, 5, 6, 8};
+    public static final int[] NEXT_4 = new int[]{1, 2, 3, 5, 7, 8, 9};
+    public static final int[] NEXT_5 = new int[]{1, 2, 3, 4, 6, 7, 8, 9};
+    public static final int[] NEXT_6 = new int[]{1, 2, 3, 5, 7, 8, 9};
+    public static final int[] NEXT_7 = new int[]{2, 4, 5, 6, 8};
+    public static final int[] NEXT_8 = new int[]{1, 3, 4, 5, 6, 7, 9};
+    public static final int[] NEXT_9 = new int[]{2, 4, 5, 6, 8};
     // Here should be arrays for all elements (1-9) in ascending order
 
     public static final int[][] NEXT_STEPS = new int[][]{
@@ -30,19 +35,24 @@ public class AndroidKeyGuard {
             NEXT_1,
             NEXT_2,
             NEXT_3,
-            NEXT_4
-            // Here should be arrays for rest elements (5-9)
+            NEXT_4,
+            NEXT_5,
+            NEXT_6,
+            NEXT_7,
+            NEXT_8,
+            NEXT_9
     };
 
+    public static void main(String[] args) {
+        AndroidKeyGuard akg = new AndroidKeyGuard();
+        long ans = akg.calcCases();
+        System.out.println("Total: " + ans);
+    }
 
     public long calcCases() {
-        long ans = 0;
         boolean[] used = new boolean[9];
-        for (int i = 4; i <= 9; i++) {
-            Arrays.fill(used, false);
-            ans += calcCases(i, 0, 0, used);
-        }
-        return ans;
+        Arrays.fill(used, false);
+        return calcCases(9, 0, 0, used);
     }
 
     public long calcCases(int total, int numOfPassed, int lastUsed, boolean[] used) {
@@ -57,7 +67,9 @@ public class AndroidKeyGuard {
             ans += calcCases(total, numOfPassed + 1, next, used);
             used[next - 1] = false;
         }
-        return ans;
+
+        long add = (numOfPassed >= 4) ? 1 : 0;  // add cases of length 4, 5, 6 and so on
+        return ans + add;
     }
 
     public List<Integer> getAllNextSteps(int lastUsed, boolean[] used) {
@@ -94,9 +106,16 @@ public class AndroidKeyGuard {
             return used[betweenVal];
         }
 
-        // check vertical. NOT FINISHED HERE
+        // check vertical
+        if (aCoord[1] == bCoord[1]) {
+            int betweenVal = 3 + aCoord[1];
+            return used[betweenVal];
+        }
 
-        // check diagonals. NOT FINISHED HERE
+        // check diagonals
+        if ((a == 0 && b == 8) || (a == 8 && b == 0)) {
+            return used[4]; // central element is 4
+        }
 
         return false;
     }
